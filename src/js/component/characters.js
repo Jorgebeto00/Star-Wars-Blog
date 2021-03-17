d018cf546f5f45d1e491adef936de46d1c8a7cd5
Jorge R <jorgebeto00@protonmail.com>
2021-03-17 02:21:28 +0000

    made overflow work

--- src/js/component/characters.js
+++ src/js/component/characters.js
@@ -1,5 +1,102 @@
-import React from 'react';
+import React from "react";
+import { Card } from "./card";
 
 export const Characters = () => {
-    
-}
+	return (
+		// <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+		// 	<div className="carousel-inner">
+		// 		<div className="carousel-item active">
+		// 			<img
+		// 				src="https://mdbootstrap.com/img/new/standard/nature/182.jpg"
+		// 				className="d-block w-100"
+		// 				alt="..."
+		// 			/>
+		// 		</div>
+		// 		<div className="carousel-item">
+		// 			<img
+		// 				src="https://mdbootstrap.com/img/new/standard/nature/182.jpg"
+		// 				className="d-block w-100"
+		// 				alt="..."
+		// 			/>
+		// 		</div>
+
+		// 			<Card />
+
+		// 	</div>
+		// 	<button
+		// 		className="carousel-control-prev"
+		// 		type="button"
+		// 		data-bs-target="#carouselExampleControls"
+		// 		data-bs-slide="prev">
+		// 		<span className="carousel-control-prev-icon" aria-hidden="true" />
+		// 		<span className="visually-hidden">Previous</span>
+		// 	</button>
+		// 	<button
+		// 		className="carousel-control-next"
+		// 		type="button"
+		// 		data-bs-target="#carouselExampleControls"
+		// 		data-bs-slide="next">
+		// 		<span className="carousel-control-next-icon" aria-hidden="true" />
+		// 		<span className="visually-hidden">Next</span>
+		// 	</button>
+		// </div>
+
+		// <div className="xx row row-cols-1 row-cols-md-3 g-4">
+		// 	<div className="col-3">
+		// 		<div className="card h-100">
+		// 			<img src="..." className="card-img-top" alt="..." />
+		// 			<div className="card-body">
+		// 				<h5 className="card-title">Card title</h5>
+		// 				<p className="card-text">
+		// 					This is a longer card with supporting text below as a natural lead-in to additional content.
+		// 					This content is a little bit longer.
+		// 				</p>
+		// 			</div>
+		// 		</div>
+		// 	</div>
+		// 	<div className="col-3">
+		// 		<div className="card h-100">
+		// 			<img src="..." className="card-img-top" alt="..." />
+		// 			<div className="card-body">
+		// 				<h5 className="card-title">Card title</h5>
+		// 				<p className="card-text">This is a short card.</p>
+		// 			</div>
+		// 		</div>
+		// 	</div>
+		// 	<div className="col-3">
+		// 		<div className="card h-100">
+		// 			<img src="..." className="card-img-top" alt="..." />
+		// 			<div className="card-body">
+		// 				<h5 className="card-title">Card title</h5>
+		// 				<p className="card-text">
+		// 					This is a longer card with supporting text below as a natural lead-in to additional content.
+		// 				</p>
+		// 			</div>
+		// 		</div>
+		// 	</div>
+		// 	<div className="col-3">
+		// 		<div className="card h-100">
+		// 			<img src="..." className="card-img-top" alt="..." />
+		// 			<div className="card-body">
+		// 				<h5 className="card-title">Card title</h5>
+		// 				<p className="card-text">
+		// 					This is a longer card with supporting text below as a natural lead-in to additional content.
+		// 					This content is a little bit longer.
+		// 				</p>
+		// 			</div>
+		// 		</div>
+		// 	</div>
+		// </div>
+
+		<div className="newOverflow">
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+		</div>
+	);
+};
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,10 +1,11 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import { Characters } from "./../component/characters";
 import "../../styles/home.scss";
 
 export const Home = () => (
 	<div>
 		<h2 className="text-danger">Characters</h2>
+		<Characters />
 		<h2 className="text-danger">Planets</h2>
 	</div>
 );
