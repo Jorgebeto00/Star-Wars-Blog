f2aadc6293d4b8dfd469c163865edf9aebd61dcc
Jorge R <jorgebeto00@protonmail.com>
2021-03-23 04:07:46 +0000

    aaawfasdfasg

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -37,25 +37,25 @@ const getState = ({ getStore, getActions, setStore }) => {
 						setStore({ planets: list2 });
 					})
 					.catch(error => console.log("error", error));
-			} //,
-			// favFunction: name => {
-			// 	const store = getStore();
-			// 	const validate = store.favlist.includes(name);
-			// 	if (validate === false) {
-			// 		const favlist = [...store.favlist, name];
-			// 		setStore({ favlist: favlist });
-			// 	}
-			// },
-			// favFunctionDelete: index => {
-			// 	const store = getStore();
-			// 	let newlist = [];
-			// 	store.favlist.map(function(item, index2) {
-			// 		if (index != index2) {
-			// 			newlist.push(item);
-			// 		}
-			// 	});
-			// 	setStore({ favlist: newlist });
-			// }
+			},
+			favFunction: name => {
+				const store = getStore();
+				const validate = store.favlist.includes(name);
+				if (validate === false) {
+					const favlist = [...store.favlist, name];
+					setStore({ favlist: favlist });
+				}
+			},
+			favFunctionDelete: index => {
+				const store = getStore();
+				let newlist = [];
+				store.favlist.map(function(item, index2) {
+					if (index != index2) {
+						newlist.push(item);
+					}
+				});
+				setStore({ favlist: newlist });
+			}
 		}
 	};
 };
